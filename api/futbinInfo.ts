422359d7d584aa3a2878f73450d4787a8f4191ec
Vivalemuc <quent.carre@gmail.com>
2021-09-27 23:35:11 +0200

    Update futbinInfo.ts

--- api/futbinInfo.ts
+++ api/futbinInfo.ts
@@ -18,7 +18,7 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
     );
      var player = information.data[0];
     
-    informations+='{"name": "'+player.Player_Name+'","defId": '+player.Player_Resource+',"buyPrice": [BUY PRICE],"sellPrice": '+player.LCPrice+',"sellBid":0,"level":"any","rarity": '+player.Rare_Type+',"style": -1,"position":"any","zone":-1, "maxPurchases":1,"buyIf":'+player.Player_Rating+', "sellIf":'+player.Player_Rating+',"buyWithStyle":false, "minContract":0}';
+    informations+='{"name": "${player.Player_Name}","defId": ${player.Player_Resource,"buyPrice": [BUY PRICE],"sellPrice": ${player.LCPrice},"sellBid":0,"level":"any","rarity":${player.Rare_Type}+',"style": -1,"position":"any","zone":-1, "maxPurchases":1,"buyIf":${player.Player_Rating}, "sellIf":${player.Player_Rating},"buyWithStyle":false, "minContract":0}';
   }
 
   res.send(informations);
