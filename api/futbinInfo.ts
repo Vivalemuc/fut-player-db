ea7ed7cd06b2a9ca93120cf0f130edd553cc7613
Vivalemuc <quent.carre@gmail.com>
2021-09-27 23:54:48 +0200

    Update futbinInfo.ts

--- api/futbinInfo.ts
+++ api/futbinInfo.ts
@@ -3,9 +3,15 @@ import Axios from "axios";
 import futbinPlayerIds from "./futbin-playerids.json";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  if (!req.query.futbinId) {
+  const allowedPlatforms = ["PC", "PS", "XB"];
+
+  if (!req.query.futbinId || !req.query.platform) {
     res.statusCode = 400;
-    res.send({ message: "Error, you must provide a valid resourceId." });
+    res.send({ message: "Error, you must provide a valid futbinId and platform." });
+    return;
+  } else if (!allowedPlatforms.includes(req.query.platform as string)) {
+    res.statusCode = 400;
+    res.send({ message: "Error, platform value is not valid try PC, PS or XB." });
     return;
   }
 
@@ -14,7 +20,7 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
   for (const futbinId of (req.query.futbinId as string).split(",")) {
 
     const { data: information } = await Axios.get(
-      `https://futbin.org/futbin/api/fetchPlayerInformation?ID=${futbinId}&platform=XB`
+      `https://futbin.org/futbin/api/fetchPlayerInformation?ID=${futbinId}&platform=${req.query.platform}`
     );
      var player = information.data[0];    
     informations+=`{"name": "${player.Player_Name}","defId": ${player.Player_Resource},"buyPrice": [BUY PRICE],"sellPrice": ${player.LCPrice},"sellBid":0,"level":"any","rarity":${player.Rare_Type},"style": -1,"position":"any","zone":-1, "maxPurchases":1,"buyIf":${player.Player_Rating}, "sellIf":${player.Player_Rating},"buyWithStyle":false, "minContract":0}`;
