72462f2cd7e74687c762f7cc322833780955b40e
Vivalemuc <quent.carre@gmail.com>
2021-09-27 23:24:24 +0200

    Create futbinInfo.js

---
+++ api/futbinInfo.js
@@ -0,0 +1,24 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import Axios from "axios";
+import futbinPlayerIds from "./futbin-playerids.json";
+
+module.exports = async (req: VercelRequest, res: VercelResponse) => {
+  if (!req.query.futbinId) {
+    res.statusCode = 400;
+    res.send({ message: "Error, you must provide a valid resourceId." });
+    return;
+  }
+
+  const informations = {};
+
+  for (const futbinId of (req.query.futbinId as string).split(",")) {
+
+    const { data: information } = await Axios.get(
+      `https://futbin.org/futbin/api/fetchPlayerInformation?ID=${futbinId}`
+    );
+
+    informations[futbinId] = information.data[0];
+  }
+
+  res.send(informations);
+};
