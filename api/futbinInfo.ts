e1d1fb36a6c2426b2f234e37d0deccee5d7fdf94
Vivalemuc <quent.carre@gmail.com>
2021-09-27 23:40:40 +0200

    Update futbinInfo.ts

--- api/futbinInfo.ts
+++ api/futbinInfo.ts
@@ -9,17 +9,15 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
     return;
   }
 
-  const informations = "";
+  var informations = "";
 
   for (const futbinId of (req.query.futbinId as string).split(",")) {
 
     const { data: information } = await Axios.get(
       `https://futbin.org/futbin/api/fetchPlayerInformation?ID=${futbinId}`
     );
-     var player = information.data[0];
-    informations += player;
-    
-    //informations+='{"name": "${player.Player_Name}","defId": ${player.Player_Resource},"buyPrice": [BUY PRICE],"sellPrice": ${player.LCPrice},"sellBid":0,"level":"any","rarity":${player.Rare_Type}+',"style": -1,"position":"any","zone":-1, "maxPurchases":1,"buyIf":${player.Player_Rating}, "sellIf":${player.Player_Rating},"buyWithStyle":false, "minContract":0}';
+     var player = information.data[0];    
+    informations+='{"name": "${player.Player_Name}","defId": ${player.Player_Resource},"buyPrice": [BUY PRICE],"sellPrice": ${player.LCPrice},"sellBid":0,"level":"any","rarity":${player.Rare_Type}+',"style": -1,"position":"any","zone":-1, "maxPurchases":1,"buyIf":${player.Player_Rating}, "sellIf":${player.Player_Rating},"buyWithStyle":false, "minContract":0}';
   }
 
   res.send(informations);
