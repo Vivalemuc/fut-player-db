cceb543e9dde3b56021ee178720f8aff6fd50d06
Vivalemuc <quent.carre@gmail.com>
2021-09-28 00:05:43 +0200

    Update futbinInfo.ts

--- api/futbinInfo.ts
+++ api/futbinInfo.ts
@@ -1,6 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import Axios from "axios";
-import futbinPlayerIds from "./futbin-playerids.json";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
   const allowedPlatforms = ["PC", "PS", "XB"];
@@ -17,11 +16,10 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
   var informations = "";
 
   for (const futbinId of (req.query.futbinId as string).split(",")) {
-
     const { data: information } = await Axios.get(
       `https://futbin.org/futbin/api/fetchPlayerInformation?ID=${futbinId}&platform=${req.query.platform}`
     );
-     var player = information.data[0];    
+    var player = information.data[0];    
     informations+=`{"name": "${player.Player_Name}","defId": ${player.Player_Resource},"buyPrice": [BUY PRICE],"sellPrice": ${player.LCPrice},"sellBid":0,"level":"any","rarity":${player.Rare_Type},"style": -1,"position":"any","zone":-1, "maxPurchases":1,"buyIf":${player.Player_Rating}, "sellIf":${player.Player_Rating},"buyWithStyle":false, "minContract":0}`;
     informations+='<br/>';
   }
