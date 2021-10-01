b1e0c38fed067a915babe0a3d3cfe506145bc9c1
Vivalemuc <quent.carre@gmail.com>
2021-10-02 00:58:01 +0200

    Update error.ts

--- api/error.ts
+++ api/error.ts
@@ -8,23 +8,22 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
     return;
   }
 
-  const informations = {};
+  const informations = [];
   var promises = [];
   
   function getInfo(id) {
-      Axios.get(
+     const { data: information } = await Axios.get(
       `https://futbin.org/futbin/api/fetchPlayerInformation?ID=${id}`
-      ).then(function (response) {
-          informations[id] = response.data[0];
-        })
+      );
+      return information.data[0];
     }
   
   for (const futbinId of (req.query.futbinId as string).split(",")) {
     promises.push(getInfo(futbinId));
   }
   
-  await Promise.all(promises).then(function (results) {
-    res.send(informations);
+  Promise.all(promises).then(function (results) {
+    res.send(results);
   });
 
 };
