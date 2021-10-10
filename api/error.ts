8a013c593e083456565f140464f5cec6237dfa63
Vivalemuc <quent.carre@gmail.com>
2021-10-10 23:49:07 +0200

    Update error.ts

--- api/error.ts
+++ api/error.ts
@@ -2,18 +2,21 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import Axios from "axios";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  if (!req.query.futbinId) {
+  if (!req.query.futbinId || !req.query.platform) {
     res.statusCode = 400;
-    res.send({ message: "Error, you must provide a valid resourceId." });
+    res.send({ message: "Error, you must provide a valid futbinId and platform." });
+    return;
+  } else if (!allowedPlatforms.includes(req.query.platform as string)) {
+    res.statusCode = 400;
+    res.send({ message: "Error, platform value is not valid try PC, PS or XB." });
     return;
   }
-
 var errorPrice = "";
 var maybeError = "";
 
   for (const futbinId of (req.query.futbinId as string).split(",")) {
     const { data: information } = await Axios.get(
-      `https://futbin.org/futbin/api/fetchPlayerInformation?ID=${futbinId}`
+      `https://futbin.org/futbin/api/fetchPlayerInformation?ID=${futbinId}&platform=${req.query.platform}``
     );
     var playerPrice;
     for(var i=0; i<information.data.length;i++){
