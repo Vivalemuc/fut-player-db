d7d35dc8b8897a5038cb37e76ec551d6c7cad01f
Vivalemuc <quent.carre@gmail.com>
2021-11-10 00:22:07 +0100

    Update errorPrice.ts

--- api/errorPrice.ts
+++ api/errorPrice.ts
@@ -3,16 +3,10 @@ import Axios from "axios";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
    
-  Axios.get(
-      `https://smp-ar.com/api/futbinPricesErrors?key=za3dh5u4da52si34a2fiezohih$zae&platform=${req.query.platform}`
-    ).then(function (response) {
-    res.send(response);
-     
-  }).catch(function (error) {
-    // handle error
-    console.log(error);
-       res.send({});
-
-  });
-
+   
+    const { data: information } = await Axios.get(
+         `https://smp-ar.com/api/futbinPricesErrors?key=za3dh5u4da52si34a2fiezohih$zae&platform=${req.query.platform}`
+    );    
+   
+   res.send(information);
 };
