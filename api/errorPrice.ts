a85dd16ac7c214e066c5eb26215249181b4944e1
Vivalemuc <quent.carre@gmail.com>
2021-11-16 20:48:20 +0100

    Update errorPrice.ts

--- api/errorPrice.ts
+++ api/errorPrice.ts
@@ -3,14 +3,29 @@ import Axios from "axios";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
    
-      const errors = [];
-    const { data: information } = await Axios.get(
-         `https://smp-ar.com/api/futbinPricesErrors?key=za3dh5u4da52si34a2fiezohih$zae&platform=${req.query.platform}`
-    );
-   for (const error of information) {
-      errors.push(error);
-   }
+      
    
-   res.send(errors);
+   axios({
+  method: 'get',
+  url:  `https://smp-ar.com/api/futbinPricesErrors?key=za3dh5u4da52si34a2fiezohih$zae&platform=${req.query.platform}`})
+  .then(function (response) {
+         res.send(response.data);
+
+  }).catch(function (error){
+       if (error.response) {
+      
+          res.send(error.response);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+                 res.send(error.request);
+
+    } else {
+      // Something happened in setting up the request that triggered an Error
+                        res.send(error.message);
+
+    }
+   });
    
 };
