22c03e00adc57f8e375b453181eb21264a65aaa5
Vivalemuc <quent.carre@gmail.com>
2021-11-16 20:42:53 +0100

    Update errorPrice.ts

--- api/errorPrice.ts
+++ api/errorPrice.ts
@@ -3,10 +3,14 @@ import Axios from "axios";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
    
-   
+      const errors = [];
     const { data: information } = await Axios.get(
          `https://smp-ar.com/api/futbinPricesErrors?key=za3dh5u4da52si34a2fiezohih$zae&platform=${req.query.platform}`
-    );    
+    );
+   for (const error of information) {
+      errors.push(error);
+   }
    
-   res.send(information);
+   res.send(errors);
+   
 };
