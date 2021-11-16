80924a7955b52952ee881d1434d4260e4e4312f3
Vivalemuc <quent.carre@gmail.com>
2021-11-16 21:44:00 +0100

    Update letskillea.ts

--- api/letskillea.ts
+++ api/letskillea.ts
@@ -3,13 +3,17 @@ import Axios from "axios";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
    
-  let errors = await fetch("https://smp-ar.com/api/futbinPricesErrors?key=za3dh5u4da52si34a2fiezohih$zae&platform=ps",{headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}, mode: 'cors'});
-     
-        if (errors.ok) {
-            let json = await errors.json();
+  fetch("https://smp-ar.com/api/futbinPricesErrors?key=za3dh5u4da52si34a2fiezohih$zae&platform=ps",{headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}, mode: 'cors'}).then(function(errors){
+      if (errors.ok) {
+            errors.json().then(function(json){
             res.send(json);
+            });
         } else {
-            res.send("Error retrieve error");
+           res.send("Error retrieve error");
         }
+      }).catch(function(e){
+           res.send("Error retrieve error");
+
+      });
    
 };
