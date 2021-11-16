a20ae9acc4ce2f1aee41bbc22adb620546070170
Vivalemuc <quent.carre@gmail.com>
2021-11-16 21:00:09 +0100

    Update letskillea.ts

--- api/letskillea.ts
+++ api/letskillea.ts
@@ -3,18 +3,12 @@ import Axios from "axios";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
    
-   Axios({
-    method: 'get',
-    url:  'https://smp-ar.com/api/futbinPricesErrors?key=za3dh5u4da52si34a2fiezohih$zae&platform=ps'})
-  .then(function (response) {
-    res.send(response.data);
-  }).catch(function (error){
-      if (error.response) {
-          res.send(error.response);
-      } else if (error.request) {
-          res.send(error.request);
-      } else {
-        res.send(error.message);
-    }
-   });
+  let res = await fetch("https://smp-ar.com/api/futbinPricesErrors?key=za3dh5u4da52si34a2fiezohih$zae&platform=ps");
+     
+        if (res.ok) {
+            let json = await res.json();
+            res.send(json);
+        } else {
+            res.send("Error retrieve error");
+        }
 };
