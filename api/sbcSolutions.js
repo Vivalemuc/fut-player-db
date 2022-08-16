88aac7cd3fe637ddb70fe9e742b2c5d7f668fa45
Vivalemuc <quent.carre@gmail.com>
2022-08-16 11:46:15 +0200

    Create sbcSolutions.js

---
+++ api/sbcSolutions.js
@@ -0,0 +1,19 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import Axios from "axios";
+
+module.exports = async (req: VercelRequest, res: VercelResponse) => {
+  const allowedPlatforms = ["PC", "PS", "XB"];
+
+  if (!req.query.challengeId) {
+    res.statusCode = 400;
+    res.send({ message: "Error, you must provide a valid challengeId." });
+    return;
+  }
+
+      const { data: sbc } = await Axios.get(`
+        https://futbin.org/futbin/api/getStcSquads?challenge=${challengeId}
+      `);
+
+    
+  res.send(sbc);
+};
