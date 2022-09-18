4e1e76bb92b9ed64710bf7f3243101e042c8d201
Vivalemuc <quent.carre@gmail.com>
2022-09-19 00:11:56 +0200

    Update playername.ts

--- api/playername.ts
+++ api/playername.ts
@@ -10,7 +10,7 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
   }
 
       const { data: players } = await Axios.get(`
-        https://futbin.org/futbin/api/searchPlayersByName?playername=${req.query.playerName}
+        https://futbin.org/futbin/api/searchPlayersByName?playername=${req.query.playerName}&year=23
       `);
 
     
