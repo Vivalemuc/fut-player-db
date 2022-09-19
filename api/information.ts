862fb534aa4d3cad87a5b83bf4d42f95e59a049d
Vivalemuc <quent.carre@gmail.com>
2022-09-19 20:09:48 +0200

    Update information.ts

--- api/information.ts
+++ api/information.ts
@@ -21,7 +21,7 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
     }
 
     const { data: information } = await Axios.get(
-      `https://futbin.org/futbin/api/fetchPlayerInformation?ID=${ids.futbin_id}`
+      `https://futbin.org/futbin/api/23/fetchPlayerInformation?ID=${ids.futbin_id}`
     );
     informations[resourceId] = information.data[0];
   }
