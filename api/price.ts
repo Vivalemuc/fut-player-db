2797c9a66ade0f8732487dc2f4cf46762ec90d60
Vivalemuc <quent.carre@gmail.com>
2022-09-22 22:12:43 +0200

    Update price.ts

--- api/price.ts
+++ api/price.ts
@@ -4,22 +4,18 @@ import Axios from "axios";
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
   const allowedPlatforms = ["PC", "PS", "XB"];
 
-  if (!req.query.resourceId || !req.query.platform) {
+  if (!req.query.resourceId) {
     res.statusCode = 400;
     res.send({ message: "Error, you must provide a valid resourceId and platform." });
     return;
-  } else if (!allowedPlatforms.includes(req.query.platform as string)) {
-    res.statusCode = 400;
-    res.send({ message: "Error, platform value is not valid try PC, PS or XB." });
-    return;
-  }
+  } 
 
   const { platform } = req.query;
   const prices = {};
 
   for (const resourceId of (req.query.resourceId as string).split(",")) {
     const { data: price } = await Axios.get(
-      `https://futbin.org/futbin/api/fetchPriceInformation?playerresource=${resourceId}&platform=${platform}`
+      `https://futbin.org/futbin/api/fetchPriceInformation?playerresource=${resourceId}&platform=PS`
     );
 
     prices[resourceId] = price;
